b2c6373933c9ff4705db40f7d4a95f1996e5d50a
Nataly Shrits <nataly87s@gmail.com>
2017-12-30 17:49:57 +0200

    NatsParser

---
+++ SimpleNatsClient.Tests/NatsParserTests.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleNatsClient.Connection;
+using Xunit;
+
+namespace SimpleNatsClient.Tests
+{
+    public class NatsParserTests
+    {
+        private static readonly TimeSpan _timeout = TimeSpan.FromMilliseconds(50);
+
+        [Fact(DisplayName = "should emit message")]
+        public async Task EmitsMessage()
+        {
+            const string message = "some message";
+            var buffer = Encoding.UTF8.GetBytes(message + "\r\n");
+
+            // Arrange
+            var parser = new NatsParser();
+            var parsedMessageTask = parser.Messages.FirstAsync()
+                .Timeout(_timeout).ToTask();
+
+            // Act
+            parser.Parse(buffer, 0, buffer.Length);
+            var parsedMessage = await parsedMessageTask;
+
+            // Assert;
+            Assert.Equal(message, parsedMessage.Message);
+            Assert.Null(parsedMessage.Payload);
+        }
+
+        [Fact(DisplayName = "should emit all messages")]
+        public async Task EmitMultipleMessages()
+        {
+            const string firstMessage = "some message";
+            const string secondMessage = "second message";
+            var buffer = Encoding.UTF8.GetBytes(firstMessage + "\r\n" + secondMessage + "\r\n");
+
+            // Arrange
+            var parser = new NatsParser();
+            var parsedMessagesTask = parser.Messages.Take(2).ToArray()
+                .Timeout(_timeout).ToTask();
+
+            // Act
+            parser.Parse(buffer, 0, buffer.Length);
+            var parsedMessages = await parsedMessagesTask;
+
+            // Assert;
+            Assert.Equal(2, parsedMessages.Length);
+            Assert.Equal(firstMessage, parsedMessages[0].Message);
+            Assert.Equal(secondMessage, parsedMessages[1].Message);
+        }
+
+        [Fact(DisplayName = "should add payload to MSG messages")]
+        public async Task ParseMsgMessage()
+        {
+            const string message = "MSG subject 12";
+            const string payload = "Hello\r\nNats!";
+            var buffer = Encoding.UTF8.GetBytes(message + "\r\n" + payload + "\r\n");
+
+            // Arrange
+            var parser = new NatsParser();
+            var parsedMessageTask = parser.Messages.FirstAsync()
+                .Timeout(_timeout).ToTask();
+
+            // Act
+            parser.Parse(buffer, 0, buffer.Length);
+            var parsedMessage = await parsedMessageTask;
+
+            // Assert;
+            Assert.Equal(message, parsedMessage.Message);
+            Assert.Equal(payload, Encoding.UTF8.GetString(parsedMessage.Payload));
+        }
+
+        [Fact(DisplayName = "should parse only selected part")]
+        public async Task ParseSelected()
+        {
+            const string message = "some message";
+            var extraBytes = Encoding.UTF8.GetBytes("garbage");
+            var messageBytes = Encoding.UTF8.GetBytes(message + "\r\n");
+            var buffer = extraBytes.Concat(messageBytes).Concat(extraBytes).ToArray();
+
+            // Arrange
+            var parser = new NatsParser();
+            var parsedMessageTask = parser.Messages.FirstAsync()
+                .Timeout(_timeout).ToTask();
+
+            // Act
+            parser.Parse(buffer, extraBytes.Length, messageBytes.Length);
+            var parsedMessage = await parsedMessageTask;
+
+            // Assert;
+            Assert.Equal(message, parsedMessage.Message);
+            Assert.Null(parsedMessage.Payload);
+        }
+
+        [Fact(DisplayName = "should parse message on multiple buffers")]
+        public async Task ParseMultipleBuffers()
+        {
+            const string message = "some message";
+            var messageBytes = Encoding.UTF8.GetBytes(message + "\r\n");            
+            var firstPart = new ArraySegment<byte>(messageBytes, 0, messageBytes.Length / 2).ToArray();
+            var secondPart = new ArraySegment<byte>(messageBytes, firstPart.Length, messageBytes.Length - firstPart.Length).ToArray();
+            
+            // Arrange
+            var parser = new NatsParser();
+            var parsedMessageTask = parser.Messages.FirstAsync()
+                .Timeout(_timeout).ToTask();
+
+            // Act
+            parser.Parse(firstPart, 0, firstPart.Length);
+            parser.Parse(secondPart, 0, secondPart.Length);
+            var parsedMessage = await parsedMessageTask;
+
+            // Assert;
+            Assert.Equal(message, parsedMessage.Message);
+            Assert.Null(parsedMessage.Payload);
+
+        }
+    }
+}
---
+++ SimpleNatsClient/Connection/NatsParser.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+
+namespace SimpleNatsClient.Connection
+{
+    public class NatsParser
+    {
+        private enum State
+        {
+            Message,
+            Payload
+        }
+
+        private readonly StringBuilder _builder;
+        
+        private State _state = State.Message;
+        private string _nextMessage;
+        private byte[] _nextPayload;
+        private int _expectedPayloadSize;
+        
+        private readonly Subject<(string Message, byte[] Payload)> _messages = new Subject<(string Message, byte[] Payload)>();
+        public IObservable<(string Message, byte[] Payload)> Messages => _messages;
+
+        public NatsParser() : this(512) {}
+        public NatsParser(int capacity)
+        {
+            _builder = new StringBuilder(capacity);
+        }
+        
+        public void Parse(byte[] buffer, int offset, int count)
+        {
+            switch (_state)
+            {
+                case State.Message: 
+                    ParseMessage(buffer, offset, count);
+                    break;
+                case State.Payload:
+                    ParsePayload(buffer, offset, count);
+                    break;
+            }
+        }
+
+        private void ParseMessage(byte[] buffer, int offset, int count)
+        {
+            for (var i = offset; i < offset + count; i++)
+            {
+                var current = buffer[i];
+                if (current == '\n')
+                {
+                    if (_builder.Length == 0) continue;
+                    
+                    var message = _builder.ToString();
+                    _builder.Clear();
+
+                    if (message.StartsWith("MSG "))
+                    {
+                        _state = State.Payload;
+                        _nextMessage = message;
+                        _expectedPayloadSize = int.Parse(message.Split(' ').Last());
+                        _nextPayload = new byte[_expectedPayloadSize];
+                        Parse(buffer, i + 1, count - i - 1);
+                        return;
+                    }
+                    
+                    _messages.OnNext((message, null));
+                }
+                else if (current != '\r')
+                {
+                    _builder.Append((char)current);
+                }
+            }
+        }
+
+        private void ParsePayload(byte[] buffer, int offset, int count)
+        {
+            Array.Copy(buffer, offset, _nextPayload, _nextPayload.Length - _expectedPayloadSize, Math.Min(count, _expectedPayloadSize));
+            offset += _expectedPayloadSize;
+            _expectedPayloadSize -= count;
+            
+            if (_expectedPayloadSize > 0) return;
+            
+            _state = State.Message;
+            _messages.OnNext((_nextMessage, _nextPayload));
+            _nextMessage = null;
+            _nextPayload = null;
+            
+            if (_expectedPayloadSize == 0) return;
+
+            count = 0 - _expectedPayloadSize;
+            _expectedPayloadSize = 0;
+            Parse(buffer, offset, count);
+        }
+    }
+}
